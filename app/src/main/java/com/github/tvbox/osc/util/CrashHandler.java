f0d9f7b66af762a7eb76cff1ee94025274878180
ubuntu <ubuntu@localhost.localdomain>
2026-09-16 02:15:23 +0000

    P0-2: 新增 CrashHandler 未捕获异常写本地日志

---
+++ app/src/main/java/com/github/tvbox/osc/util/CrashHandler.java
@@ -0,0 +1,79 @@
+package com.github.tvbox.osc.util;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * @author pj567
+ * @date :2026/09/16
+ * @description: 未捕获异常本地日志兜底
+ */
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    private static final String TAG = "CrashHandler";
+    private static final String CRASH_FILE_NAME = "crash.log";
+    private final Context context;
+    private final Thread.UncaughtExceptionHandler defaultHandler;
+
+    public CrashHandler(Context context) {
+        this.context = context.getApplicationContext();
+        this.defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable throwable) {
+        try {
+            saveCrashLog(thread, throwable);
+        } catch (Throwable ignored) {
+        }
+        if (defaultHandler != null) {
+            defaultHandler.uncaughtException(thread, throwable);
+        } else {
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    }
+
+    private void saveCrashLog(Thread thread, Throwable throwable) {
+        File dir = context.getFilesDir();
+        if (dir == null) {
+            dir = context.getExternalFilesDir(null);
+        }
+        if (dir == null) {
+            Log.e(TAG, "no writable dir to store crash log, dir=null");
+            return;
+        }
+        File file = new File(dir, CRASH_FILE_NAME);
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.getDefault()).format(new Date());
+        try {
+            FileWriter writer = new FileWriter(file, true);
+            PrintWriter pw = new PrintWriter(writer);
+            pw.println("=================== " + time + " ===================");
+            pw.println("Thread: " + thread.getName() + "(" + thread.getId() + ")");
+            pw.println(throwable.toString());
+            for (StackTraceElement element : throwable.getStackTrace()) {
+                pw.println("\tat " + element.toString());
+            }
+            Throwable cause = throwable.getCause();
+            while (cause != null) {
+                pw.println("Caused by: " + cause.toString());
+                for (StackTraceElement element : cause.getStackTrace()) {
+                    pw.println("\tat " + element.toString());
+                }
+                cause = cause.getCause();
+            }
+            pw.println();
+            pw.flush();
+            pw.close();
+            Log.i(TAG, "crash log saved to " + file.getAbsolutePath());
+        } catch (Throwable e) {
+            Log.e(TAG, "failed to save crash log: " + e.getMessage());
+        }
+    }
+}
